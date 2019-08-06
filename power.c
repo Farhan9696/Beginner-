a7d64fb566848e6181f03cefe2e99f4abef66287
Farhan9696 <37727645+Farhan9696@users.noreply.github.com>
2019-08-06 15:54:03 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,24 @@
+#include<stdio.h> 
+ 
+int main(void)
+{       
+    int base, exponent, result = 1;
+    
+    printf("Enter base: ");
+    scanf("%d", &base);
+    
+    printf("Enter exponent: ");
+    scanf("%d", &exponent);
+    
+    int i = 1;
+    
+    while(i <= exponent)
+    {
+        result *= base;
+        i++;
+    }
+    
+    printf("%d^%d = %d", base, exponent, result);
+    
+    return 0; 
+}
